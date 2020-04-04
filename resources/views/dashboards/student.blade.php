25029b23f35ceed33a3dd867182824bf82cd5887
meesterdaaf <d.vanoudheusden@rocva.nl>
2020-04-04 16:32:53 +0200

    ugly interface but it works

--- resources/views/dashboards/student.blade.php
+++ resources/views/dashboards/student.blade.php
@@ -16,26 +16,22 @@
     {{-- {{dd($modules)}} --}}
     @foreach ($modules as $module)
     {{-- {{dd($user->modules->find($module->id)['id'])}} --}}
-    <div class="col">
-        @if($user->modules()->find($module->id) != null )
-        @if($module->id == $user->modules()->find($module->id)['id'] )
-        
+    {{-- <div class="col"> --}}
+        {{-- {{dd($user->modules)}} --}}
+        {{-- {{dd($user->modules()->where('module_id', $module->id)->first()->pivot->status)}} --}}
+        {{-- @if($user->modules()->find($module->id) != null ) --}}
+        @php $levelStatus = $user->modules()->where('module_id', $module->id)->first()->pivot->status @endphp
+        @if($levelStatus == 1 )
         <a href="{{route('modules.show', ['module'=> $module->slug ])}}" class="card module-to-choose">
-            @endif
-            <div class="card-body d-flex flex-column align-items-center justify-content-center"
-                style="width:12rem;height:12rem;">
+        @endif
+            <div class="card-body d-flex flex-column align-items-center justify-content-center @if($levelStatus == 0) bg-danger @elseif($levelStatus == 1) bg-success @else bg-info @endif" 
+                style="width:12rem;height:12rem;" >
                 <h5 class="card-title h1">{{$module->name}}</h5>
             </div>
-            @if($module->id == $user->modules->find($module->id)['id'] )
+        @if($levelStatus == 1 )
         </a>
         @endif
-        @else
-        <div class="card-body d-flex flex-column align-items-center justify-content-center"
-                style="width:12rem;height:12rem;">
-                <h5 class="card-title h1">{{$module->name}}</h5>
-            </div>
-        @endif
-    </div>
+    {{-- </div> --}}
     @endforeach
 </div>
 
