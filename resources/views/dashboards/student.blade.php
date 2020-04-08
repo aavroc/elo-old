73d48c9d4d58ff986fbae77d5f9d506fee4d60bf
meesterdaaf <d.vanoudheusden@rocva.nl>
2020-04-08 13:36:47 +0200

    student view restored

--- resources/views/dashboards/student.blade.php
+++ resources/views/dashboards/student.blade.php
@@ -15,7 +15,25 @@ Dashboard
     <!-- Write page content code here -->
     <!-- Start XP Row -->    
     <div class="row">
-    @David deze pagina geeft een foutmelding (heet nu bij mij student1.blade)
+    @foreach ($modules as $module)
+    {{-- {{dd($user->modules->find($module->id)['id'])}} --}}
+    {{-- <div class="col"> --}}
+        {{-- {{dd($user->modules)}} --}}
+        {{-- {{dd($user->modules()->where('module_id', $module->id)->first()->pivot->status)}} --}}
+        {{-- @if($user->modules()->find($module->id) != null ) --}}
+        @php $levelStatus = $user->modules()->where('module_id', $module->id)->first()->pivot->status @endphp
+        @if($levelStatus == 1 )
+        <a href="{{route('modules.show', ['module'=> $module->slug ])}}" class="card module-to-choose">
+        @endif
+            <div class="card-body d-flex flex-column align-items-center justify-content-center @if($levelStatus == 0) bg-danger @elseif($levelStatus == 1) bg-success @else bg-info @endif" 
+                style="width:12rem;height:12rem;" >
+                <h5 class="card-title h1">{{$module->name}}</h5>
+            </div>
+        @if($levelStatus == 1 )
+        </a>
+        @endif
+    {{-- </div> --}}
+    @endforeach
     </div>
     <!-- End XP Row -->  
 </div>
