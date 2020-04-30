bfea735ccbefc833e5a5e6dfe4cfe888c31373dd
Aldo Vogel <a.vogel@rocva.nl>
2020-04-30 20:13:01 +0200

    updated to reflect changed model

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,10 +19,19 @@ use Illuminate\Support\Str;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'firstname' => $faker->firstName,
+        'prefix' => "",
+        'lastname' => $faker->lastName,
+        'studentnr' => $faker->numberBetween($min = 00000, $max = 99999),
         'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'role' => 3,
+        'github_nickname' => $faker->userName,
+        'github_email' => $faker->unique()->safeEmail,
+        'classroom' => $faker->randomElement($array = array('LCTAOO9A','LCTAOO9C','LCTAOO9D')),
+        'status_id' => $faker->numberBetween($min = 1, $max = 2),
+        'email_verified_at' => now(),
         'remember_token' => Str::random(10),
+        
     ];
 });
