83b6465e978d6edab75bd354490902c1618d95c0
meesterdaaf <d.vanoudheusden@rocva.nl>
2020-05-12 11:14:44 +0200

    default waardes aangepast van tabel

--- public/assets/js/init/table-datatable-init.js
+++ public/assets/js/init/table-datatable-init.js
@@ -1,19 +1,21 @@
 "use strict";
-$(document).ready(function(){
-
+$(document).ready(function () {
     /* -----  Table - Datatable  ----- */
-    $('#datatable').DataTable();
+    $("#datatable").DataTable();
 
-    $('#xp-default-datatable').DataTable( {
-        "order": [[ 3, "desc" ]]
-    } );
-    
-    var table = $('#datatable-buttons').DataTable({
-        lengthChange: false,
-        buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
+    $("#xp-default-datatable").DataTable({
+        order: [[3, "desc"]],
+        pageLength: 30,
+        lengthMenu: [10, 25, 30, 50],
     });
 
-    table.buttons().container()
-    .appendTo('#datatable-buttons_wrapper .col-md-6:eq(0)');
+    var table = $("#datatable-buttons").DataTable({
+        lengthChange: false,
+        buttons: ["copy", "csv", "excel", "pdf", "print"],
+    });
 
+    table
+        .buttons()
+        .container()
+        .appendTo("#datatable-buttons_wrapper .col-md-6:eq(0)");
 });
