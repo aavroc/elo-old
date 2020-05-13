aec7a412ddd6bae068e5d8cdfff9fa9787d79ec0
meesterdaaf <d.vanoudheusden@rocva.nl>
2020-05-13 14:29:08 +0200

    view aagepast

--- resources/views/layouts/charts/classroom-total-tasks-completed-per-student.blade.php
+++ resources/views/layouts/charts/classroom-total-tasks-completed-per-student.blade.php
@@ -1,22 +1,18 @@
-<div class="row">
-    <div class="col-lg-6">
-        <div class="card m-b-30">
-            <div class="card-header bg-white">
-                <h5 class="card-title text-black">Taken Overzicht</h5>
-            </div>
-            <div class="card-body">
-                @foreach($users as $user)
-                    <div class="xp-progressbar">
-                        @php
-                            $number = \DB::table('users_tasks')->where('evaluation', 1)->where('user_id', $user->id)->count();
-                        @endphp
-                        <p class=""><a href="{{route('users.show', $user)}}">{{$user->firstname}}</a><span class="pull-right">{{$number}}</span></p>
-                        <div class="progress m-b-20" style="height: 10px;">
-                            <div class="progress-bar bg-primary" role="progressbar" style="width: {{$number}}%" aria-valuenow="{{$number}}" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div>
+    <div class="card m-b-30">
+        <div class="card-header bg-white">
+            <h5 class="card-title text-black">Taken Overzicht</h5>
+        </div>
+        <div class="card-body">
+            @foreach($users as $user)
+                <div class="xp-progressbar">
+                    @php
+                        $number = \DB::table('users_tasks')->where('evaluation', 1)->where('user_id', $user->id)->count();
+                    @endphp
+                    <p class=""><a href="{{route('users.show', $user)}}">{{$user->firstname}}</a><span class="pull-right">{{$number}}</span></p>
+                    <div class="progress m-b-20" style="height: 10px;">
+                        <div class="progress-bar bg-primary" role="progressbar" style="width: {{$number}}%" aria-valuenow="{{$number}}" aria-valuemin="0" aria-valuemax="100"></div>
                     </div>
-                @endforeach
-            </div>
+                </div>
+            @endforeach
         </div>
     </div>
-</div>
